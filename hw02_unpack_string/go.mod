485e0840fe97fa8c936debe785caf20375e6eb53
Sergei Fursov <sergei.fursov@softgamings.com>
2024-12-22 23:22:46 +0300

    2nd hw

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/fursserg/otus_hw/hw02_hello_otus
 
 go 1.22
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,90 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func main() {
+	testStrings := []string{
+		`a4bc2d5e`,
+		"abcd",
+		"3abc",
+		"45",
+		`aaa10b`,
+		"aaa0b",
+		"d0f4",
+		"d10f4",
+		`qwe\4\5`,
+		`qwe\45`,
+		`qwe\\5`,
+		`qw\ne`,
+		`qwe\\\3`,
+	}
+
+	for _, str := range testStrings {
+		res, err := Unpack(str)
+		if err != nil {
+			res = fmt.Sprintf("%s", err)
+		}
+
+		fmt.Printf("%s => %s\n", str, res)
+	}
+}
+
+func Unpack(str string) (string, error) {
+	var res []string
+	runes := []rune(str)
+
+	for i := 0; i < len(runes); i++ {
+		symbol := string(runes[i])
+
+		next := ""
+		if len(runes) > i+1 {
+			next = string(runes[i+1])
+		}
+
+		isNum, num := isNumeric(symbol)
+		isNextNum, _ := isNumeric(next)
+
+		if symbol == "\\" {
+			if next == "" {
+				return "", ErrInvalidString
+			}
+
+			if !isNextNum && next != "\\" {
+				return "", ErrInvalidString
+			}
+
+			res = append(res, next)
+			i++
+			continue
+		}
+
+		if isNum {
+			if i == 0 || isNextNum {
+				return "", ErrInvalidString
+			}
+
+			res = append(res[:len(res)-1], strings.Split(strings.Repeat(res[len(res)-1], num), "")...)
+			continue
+		}
+
+		res = append(res, symbol)
+	}
+
+	return strings.Join(res, ""), nil
+}
+
+func isNumeric(s string) (bool, int) {
+	num, err := strconv.Atoi(s)
+	if err == nil {
+		return true, num
+	}
+
+	return false, 0
 }
