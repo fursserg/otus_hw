0dd796a1ddd647928f3f5a3e1df10d4626ccc897
Sergei Fursov <sergei.fursov@softgamings.com>
2025-01-14 18:37:36 +0300

    remove "no-lint" tag

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -3,7 +3,7 @@ package hw03frequencyanalysis
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require" //nolint:depguard
+	"github.com/stretchr/testify/require"
 )
 
 // Change to true if needed.
