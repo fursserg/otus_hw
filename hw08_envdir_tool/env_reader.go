43fb99cbb64637fd41a06c0ac4087a2537e79a33
Sergei Fursov <sergei.fursov@softgamings.com>
2025-05-12 21:56:58 +0300

    HW8

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,38 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("ReadDir error: %w", err)
+	}
+
+	vars := map[string]EnvValue{}
+
+	for _, file := range files {
+		fileName := file.Name()
+
+		if !strings.Contains(fileName, "=") && !file.IsDir() {
+			data, err := os.ReadFile(dir + "/" + fileName)
+			if err != nil {
+				return nil, fmt.Errorf("ReadDir read file %s error: %w", fileName, err)
+			}
+
+			_, existsEnv := os.LookupEnv(fileName)
+			env := EnvValue{NeedRemove: existsEnv}
+
+			if len(data) > 0 {
+				data = bytes.SplitN(data, []byte("\n"), 2)[0]
+				data = bytes.TrimRight(data, "\t ")
+				data = bytes.ReplaceAll(data, []byte{0}, []byte("\n"))
+
+				env.Value = string(data)
+				env.NeedRemove = false
+			}
+
+			vars[fileName] = env
+		}
+	}
+
 	// Place your code here
-	return nil, nil
+	return vars, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Printf("you need to pass at least two arguments, passed: %d\n", len(os.Args))
+		return
+	}
+
+	vars, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	RunCmd(os.Args[2:], vars)
 }
